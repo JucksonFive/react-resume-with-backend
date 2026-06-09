fa69f8173916f88b583bb014d1be488535e5e372
jplappa <jukka-pekka.lappalainen@sanoma.com>
2026-06-09 12:36:59 +0300

    ci: update Lighthouse CI version and remove package-lock.json

--- react-resume-template/next.config.js
+++ react-resume-template/next.config.js
@@ -1,8 +1,12 @@
 /* eslint-env node */
 
+const path = require('path');
+
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
-  turbopack: {},
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
   compress: true,
   generateEtags: true,
   pageExtensions: ['tsx', 'mdx', 'ts'],
